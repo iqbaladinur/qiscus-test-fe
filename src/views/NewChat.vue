b2a1f9f1f96e5385c78069587eb4136889322f56
Iqbal Adi Nurmansyah <iqbaladinur@gmail.com>
2020-09-08 14:48:06 +0700

    added new chat

--- src/views/NewChat.vue
+++ src/views/NewChat.vue
@@ -2,7 +2,18 @@
   <div class="h-full">
     <top-navbar :is-main-frame="false"/>
     <ul v-if="contactList.length > 0" class="list-container overflow-y-auto h-custom">
-
+      <contact
+        v-for="(contact, idx) in contactList"
+        :key="idx"
+        :username="contact.username"
+        :userId="contact.email"
+        :avatar="contact.avatar_url"
+      />
+      <li class="text-center my-2">
+        <button class="text-xs rounded bg-blue-400 py-2 px-4" @click="nextPage">
+          {{ loadMoreText }}
+        </button>
+      </li>
     </ul>
   </div>
 </template>
@@ -19,7 +30,32 @@ export default {
   },
   data() {
     return {
-      contactList: []
+      perPage: 20,
+      page: 1,
+      contactList: [],
+      loadMoreText: 'Load more',
+    }
+  },
+  mounted() {
+    this.loadContactList()
+  },
+  methods: {
+    loadContactList() {
+      const ctx = this;
+      ctx.qiscus.getUsers('', ctx.page, ctx.limit)
+        .then(function ({ meta, users }) {
+          ctx.loadMoreText = 'Load more'
+          ctx.contactList = [...ctx.contactList, ...users]
+        })
+        .catch(function (error) {
+          ctx.loadMoreText = 'Try again'
+          console.error(error)
+        })
+    },
+    nextPage() {
+      this.page += 1
+      this.loadMoreText = 'Loading..'
+      this.loadContactList()
     }
   }
 }
